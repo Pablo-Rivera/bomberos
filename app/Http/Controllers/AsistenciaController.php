9b08994b7c813e7b71b0edd31e53fdc13e054c18
Pablo <paliyo@gmail.com>
2018-05-22 16:44:07 -0300

    arreglos

--- app/Http/Controllers/AsistenciaController.php
+++ app/Http/Controllers/AsistenciaController.php
@@ -22,7 +22,7 @@ class AsistenciaController extends Controller
 
     public function index()
     {
-      $reuniones=asistencia::select('fecha_reunion')->orderBy('fecha_reunion', 'desc')->groupBy('fecha_reunion')->take(10)->get();
+      $reuniones=asistencia::select('fecha_reunion')->orderBy('fecha_reunion', 'asc')->groupBy('fecha_reunion')->take(10)->get();
       return view('asistencia/listar',compact('reuniones'));
     }
 
